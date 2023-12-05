48c6a42d107f93689c99eceb54e2920c2806c67e
Tashfeen Chohan <chohantashfeen@gmail.com>
2023-12-06 00:27:41 +0500

    Design Category Schema Model

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT  || 3000;
 
-app.use(express.json())
+// DATABASE CONNECTION
+mongoose
+  .connect("mongodb://127.0.0.1/Blogify")
+  .then(() => console.log("Database connected successfully!"))
+  .catch((error) => console.log("Could not connect to the database : ", error));
 
-app.listen(() => console.log("Server is listening on port 3000!"))
+// MIDDLEWARE FUNCTIONS
+app.use(express.json());
+
+
+app.listen(() => console.log(`Server is listening on port ${PORT}!`));
---
+++ models/Category.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Joi = require("joi")
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: { type: String, unique: true, require: true },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model("Category", categorySchema)
+
+// JOI VALIDATION
+const validateCategory = (category) => {
+  const schema = Joi.object({
+    name: Joi.string().min(2).required()
+  })
+  return schema.validate(category)
+}
+
+module.exports = {
+  Category,
+  validateCategory
+}
+
+
