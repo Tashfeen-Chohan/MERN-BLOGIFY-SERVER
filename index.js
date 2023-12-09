8e1997d2416c77b506ead560c3e9c75244b4d7a2
Tashfeen Chohan <chohantashfeen@gmail.com>
2023-12-10 01:16:55 +0500

    Multer configuration for Post Cover

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser")
+const multer = require("multer")
+const asyncHandler = require("express-async-handler")
 const PORT = process.env.PORT  || 3000;
 
 // DATABASE CONNECTION
@@ -10,6 +12,22 @@ mongoose
   .connect("mongodb://127.0.0.1/Blogify")
   .then(() => console.log("Database connected successfully!"))
   .catch((error) => console.log("Could not connect to the database : ", error));
+
+const storage = multer.storage({
+  destination: (req, file, cb) => {
+    cb(null, "Images")
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name)
+  }
+})  
+
+const upload = multer({storage: storage})
+app.post("/upload", upload.single("file"), asyncHandler(async (req, res) => {
+  const file = req.file
+  if (!file) return res.status(400).send({message: "No file provided!"})
+  res.status(200).send({message: "Post cover image uploaded successfully!"})
+}))
 
 // MIDDLEWARE FUNCTIONS
 app.use(express.json());
